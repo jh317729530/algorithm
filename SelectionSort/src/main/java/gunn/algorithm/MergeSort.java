6eca7653af5be4e8c9113fdfbd68598ff0fa6e36
ganjunhui <junhui_g@gzleihou.cn>
2018-10-24 15:25:21 +0800

    自底向上的归并排序

--- SelectionSort/src/main/java/gunn/algorithm/MergeSort.java
+++ SelectionSort/src/main/java/gunn/algorithm/MergeSort.java
@@ -49,23 +49,35 @@ public class MergeSort implements Sort {
         merge(a, 0, mid, a.length - 1);
     }
 
+    @Override
+    public void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sortBU(a);
+    }
+
     /**
      * 自顶向下的归并排序
      * @param a
      */
-    @Override
-    public void sort(Comparable[] a) {
-        aux = new Comparable[a.length];
-        sort(a, 0, a.length - 1);
-    }
-
-    private void sort(Comparable[] a, int lo, int hi) {
+    private void sortTD(Comparable[] a, int lo, int hi) {
         if (hi <= lo) {
             return;
         }
         int mid = lo + (hi - lo) / 2;
-        sort(a, lo, mid);
-        sort(a, mid + 1, hi);
+        sortTD(a, lo, mid);
+        sortTD(a, mid + 1, hi);
         merge(a,lo,mid,hi);
+    }
+
+    /**
+     * 自底向上的归并排序
+     */
+    public void sortBU(Comparable[] a) {
+        int N = a.length;
+        for (int sz = 1; sz < N; sz = sz + sz) {
+            for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+            }
+        }
     }
 }
